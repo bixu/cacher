93ee4b0ee574554855c4d0c84489e53934d77552
Blake Irvin <blake.irvin@gmail.com>
2018-08-29 16:53:39 +0200

    set `XDG_CACHE_HOME` to store caches in our Habitat artifacts cache
    This can potentially speed up builds by up to 20%.

--- cacher/plan.sh
+++ cacher/plan.sh
@@ -4,17 +4,16 @@ pkg_version="0.1.0"
 pkg_maintainer="Blake Irvin <blake.irvin@gmail.com>"
 pkg_license=("MIT")
 pkg_lib_dirs=("lib")
-pkg_deps=(
-  core/tar
-  core/gzip
-  core/sed
-)
+
+do_setup_environment() {
+  mkdir --parents "/hab/cache/artifacts/studio_cache"
+  set_runtime_env XDG_CACHE_HOME "/hab/cache/artifacts/studio_cache"
+}
 
 do_build() {
   return 0
 }
 
 do_install() {
-  cp $PLAN_CONTEXT/lib/functions $PREFIX/lib/functions
-  return $?
+  return 0
 }
