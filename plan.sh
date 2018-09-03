c70bf971d61f98139561b15c2785c1979ab2b4de
Blake Irvin <blake.irvin@gmail.com>
2018-09-03 11:35:58 +0200

    support caching for Go deps
    Also do some refactoring for clarity.

--- plan.sh
+++ plan.sh
@@ -1,6 +1,6 @@
 pkg_origin=bixu
 pkg_name=cacher
-pkg_version="0.1.0"
+pkg_version="0.2.0"
 pkg_maintainer="Blake Irvin <blake.irvin@gmail.com>"
 pkg_license=("MIT")
 pkg_deps=(
@@ -9,9 +9,17 @@ pkg_deps=(
 
 
 do_setup_environment() {
-  mkdir --parents                  "/hab/cache/artifacts/studio_cache"
-  set_runtime_env XDG_CACHE_HOME   "/hab/cache/artifacts/studio_cache"
-  set_runtime_env npm_config_cache "/hab/cache/artifacts/studio_cache"
+  # enable caching for Go dependencies
+  mkdir --parents                  "/hab/cache/artifacts/studio_cache/go"
+  set_runtime_env GOPATH           "/hab/cache/artifacts/studio_cache/go"
+
+  # enable caching for NPM modules
+  mkdir --parents                  "/hab/cache/artifacts/studio_cache/npm"
+  set_runtime_env npm_config_cache "/hab/cache/artifacts/studio_cache/npm"
+
+  # enable caching for Pip modules
+  mkdir --parents                  "/hab/cache/artifacts/studio_cache/pip"
+  set_runtime_env XDG_CACHE_HOME   "/hab/cache/artifacts/studio_cache/pip"
 }
 
 do_build() {
@@ -25,7 +33,8 @@ do_install() {
 do_end() {
   build_line ""
   build_line "Cache settings:"
-  build_line "$(hab pkg exec "$pkg_origin/$pkg_name" env | grep XDG_CACHE_HOME)"
+  build_line "$(hab pkg exec "$pkg_origin/$pkg_name" env | grep GOPATH)"
   build_line "$(hab pkg exec "$pkg_origin/$pkg_name" env | grep npm_config_cache)"
+  build_line "$(hab pkg exec "$pkg_origin/$pkg_name" env | grep XDG_CACHE_HOME)"
   return $?
 }
