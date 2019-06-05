a0bba44338c437fc271fa82ce4dea56d11b21698
Blake Irvin <irvin@Halation-Effect.local>
2019-06-05 17:34:06 +0200

    enable proper inheritance of caching variables

--- plan.sh
+++ plan.sh
@@ -1,30 +1,26 @@
 pkg_origin=bixu
 pkg_name=cacher
-pkg_version='0.3.0'
-pkg_maintainer='Blake Irvin <blakeirvin@me.com>'
-pkg_license=('MIT')
+pkg_version="1.0.0"
+pkg_maintainer="Blake Irvin <blakeirvin@me.com>"
+pkg_license=("MIT")
 pkg_deps=(
-  'core/coreutils'
+  "core/coreutils"
 )
 
 
 do_setup_environment() {
   # enable caching for Go dependencies
-  mkdir --parents '/hab/cache/artifacts/studio_cache/go'
-  set_buildtime_env GOPATH '/hab/cache/artifacts/studio_cache/go'
+  set_runtime_env GOPATH "/hab/cache/artifacts"
 
   # enable caching for NPM modules
-  mkdir --parents '/hab/cache/artifacts/studio_cache/npm'
-  set_buildtime_env npm_config_cache '/hab/cache/artifacts/studio_cache/npm'
+  set_runtime_env npm_config_cache "/hab/cache/artifacts"
 
   # enable caching for Pip modules
-  mkdir --parents '/hab/cache/artifacts/studio_cache/pip'
-  set_buildtime_env PIP_DOWNLOAD_CACHE '/hab/cache/artifacts/studio_cache/pip'
+  set_runtime_env XDG_CACHE_HOME "/hab/cache/artifacts"
   
   # enable caching for Ruby gems
-  mkdir --parents '/hab/cache/artifacts/studio_cache/rubygems'
-  set_buildtime_env GEM_HOME '/hab/cache/artifacts/studio_cache/rubygems'
-  set_buildtime_env GEM_PATH '/hab/cache/artifacts/studio_cache/rubygems'
+  set_runtime_env GEM_HOME "/hab/cache/artifacts"
+  set_runtime_env GEM_PATH "/hab/cache/artifacts"
   return $?
 }
 
@@ -34,15 +30,4 @@ do_build() {
 
 do_install() {
   return 0
-}
-
-do_end() {
-  build_line ""
-  build_line "Cache settings:"
-  build_line "$(hab pkg exec "$pkg_origin/$pkg_name" env | grep GEM_HOME)"
-  build_line "$(hab pkg exec "$pkg_origin/$pkg_name" env | grep GEM_PATH)"
-  build_line "$(hab pkg exec "$pkg_origin/$pkg_name" env | grep GOPATH)"
-  build_line "$(hab pkg exec "$pkg_origin/$pkg_name" env | grep npm_config_cache)"
-  build_line "$(hab pkg exec "$pkg_origin/$pkg_name" env | grep PIP_DOWNLOAD_CACHE)"
-  return $?
 }
